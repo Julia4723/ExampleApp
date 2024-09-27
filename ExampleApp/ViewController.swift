f03af1e880711861f1b5179fcae113111b77bf57
My <uk472363@yanex.ru>
2024-09-27 09:12:29 +0300

    Initial Commit

---
+++ ExampleApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ExampleApp
+//
+//  Created by user on 27.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let myName = "Julius"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        print("I am \(myName). I am a developer.")
+    }
+
+
+}
+
