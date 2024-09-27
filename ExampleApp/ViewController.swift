08bd3d84d527c82bdc1418df76b23ec1ccd7aa69
My <uk472363@yanex.ru>
2024-09-27 09:12:58 +0300

    Commit

--- ExampleApp/ViewController.swift
+++ ExampleApp/ViewController.swift
@@ -14,7 +14,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         print("I am \(myName). I am a developer.")
     }
 
